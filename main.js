69a5aa312198f44f4baf8677c12a69f75722e3a2
justynasadlak <justynasadlak10@gmail.com>
2021-01-29 18:45:21 +0100

    change http to https

--- main.js
+++ main.js
@@ -715,7 +715,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    apiUrl: 'http://192.168.0.76:10000'
+    apiUrl: 'https://192.168.0.76:10000'
 };
 /*
  * For easier debugging in development mode, you can import the following file
